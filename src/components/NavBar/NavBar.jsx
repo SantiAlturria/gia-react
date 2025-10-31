573090d07c8cb00db7882679cb3711e4c9424e44
santialturria <alturriasantiagoezequiel@gmail.com>
2025-10-31 02:04:37 -0300

    estilizando navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,37 +2,22 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import CartWidget from "../CartWidget";
 import logo from "../../assets/logodonas.jpg";
-
-
-const categories = [
-  { id: "all", label: "Todos" },
-  { id: "donas", label: "Donas" },
-  { id: "rosquitas", label: "Rosquitas" },
-  { id: "combo", label: "Combos" },
-];
+import "../NavBar/NavBar.css";
 
 export default function NavBar() {
   return (
-    <header className="navbar">
-      <div className="brand">
-        <NavLink to="/">
-          <img src={logo} alt="Rosquitas & Donas" style={{ height: 48 }} />
-        </NavLink>
-      </div>
-      <nav>
-        <ul className="nav-links">
-          <li><NavLink to="/" end>Home</NavLink></li>
-          {categories.map(cat => (
-            <li key={cat.id}>
-              {}
-              <NavLink to={cat.id === "all" ? "/" : `/category/${cat.id}`}>
-                {cat.label}
-              </NavLink>
-            </li>
-          ))}
-        </ul>
-      </nav>
-      <CartWidget />
-    </header>
+    <nav className="navbar">
+  <a href="/" className="logo">
+    <img src="{logo}" alt="Rosquitas & Donas" />
+    Rosquitas & Donas
+  </a>
+  <div className="nav-links">
+    <a href="#catalogo">Catálogo</a>
+    <a href="#nosotros">Sobre Nosotros</a>
+    <a href="#contacto">Contacto</a>
+    <div className="cart">🛒</div>
+  </div>
+</nav>
+
   );
 }
